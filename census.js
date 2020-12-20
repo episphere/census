fc52e6318c52b11ebe67eda1f15ba15b0e11171b
Jonas Almeida <jalmeida@mathbiol.org>
2020-12-19 21:55:53 -0500

    support for census.key

--- census.js
+++ census.js
@@ -3,12 +3,14 @@ console.log('census.js loaded')
 census={}
 
 census.get=async(q='/data/',apiURL='https://us-central1-nih-nci-dceg-episphere-dev.cloudfunctions.net/episphere_census')=>{ // default returns catalog, could also be, for example census.get('/data/2018/acs/acs1/subject?get=NAME,group(S0101)&for=us:1')
+    let url=''
     if(census.key){
-        apiURL='https://api.census.gov'
-    }
-    let url=`${apiURL}?${encodeURIComponent(q)}`
-    if(!q.match(/\?/g)){
-        url=`${apiURL}?${encodeURIComponent(q)}?date=${Date()}`
+        url=`https://api.census.gov${q}?key=${census.key}`
+    }else{
+        url=`${apiURL}?${encodeURIComponent(q)}`
+        if(!q.match(/\?/g)){
+            url=`${apiURL}?${encodeURIComponent(q)}?date=${Date()}`
+        }
     }
     //console.log(`calling ${url}`)
     let res = await (await fetch(url)).text()
@@ -20,6 +22,10 @@ census.get=async(q='/data/',apiURL='https://us-central1-nih-nci-dceg-episphere-d
         console.log(err,res)
         }
     return res
+}
+
+census.help=function(){
+    window.open('https://observablehq.com/@episphere/census')
 }
 
 // example
